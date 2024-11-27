6bc85b946e337a7d2d4b080801b8153b159d5986
zylxrd <2302463A@student.tp.edu.sg>
2024-11-27 10:30:05 +0800

    Added custom identity logic

--- CarRentalManagement/Program.cs
+++ CarRentalManagement/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<CarRentalManagementUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<CarRentalManagementContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
